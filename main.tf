03251237163d3d976648d15f3081f9ac690abd2d
SruthyManohar <mail2sruthymanohar@gmail.com>
2021-09-03 11:26:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,6 @@ terraform {
         bucket      = "remotetfstate"
         key         = "terraform/terraform.tfstate"
         region      = "ap-south-1"
-        encrypt     = false
+        encrypt     = true
         }
 }
